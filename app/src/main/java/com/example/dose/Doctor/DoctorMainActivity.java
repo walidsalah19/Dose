d74db19deb362e3222673667ab8c4b6fc6cdb23c
walid salah <walidsalah9585@gmail.com>
2023-03-25 04:18:31 +0200

    dose

---
+++ app/src/main/java/com/example/dose/Doctor/DoctorMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dose.Doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dose.R;
+
+public class DoctorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_main);
+    }
+}
---
+++ app/src/main/java/com/example/dose/pharmaceutical/PharmaceuticalMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dose.pharmaceutical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dose.R;
+
+public class PharmaceuticalMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pharmaceutical_main);
+    }
+}
