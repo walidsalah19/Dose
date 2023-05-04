a1535d456c00f5327a925f0a35190bf74ccc3ca4
walid salah <walidsalah9585@gmail.com>
2023-05-04 19:25:09 +0300

    update

--- app/src/main/java/com/example/dose/User/Adapter/DisplayAlarms.java
+++ app/src/main/java/com/example/dose/User/Adapter/DisplayAlarms.java
@@ -1,6 +1,76 @@
 package com.example.dose.User.Adapter;
 
-public class DisplayAlarms {
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
 
-    public 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dose.Common;
+import com.example.dose.Models.Alarm;
+import com.example.dose.Models.User;
+import com.example.dose.R;
+
+import java.util.ArrayList;
+
+public class DisplayAlarms extends RecyclerView.Adapter<DisplayAlarms.help>{
+    private OnItemClickListener mListener;
+    public interface OnItemClickListener{
+        void onclick(int position);
+    }
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener=listener;
+    }
+    private ArrayList<Alarm> alarms;
+
+    public DisplayAlarms(ArrayList<Alarm> alarms) {
+        this.alarms = alarms;
+    }
+
+    @NonNull
+    @Override
+    public help onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_alarms,parent,false);
+        return new help(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull help holder, @SuppressLint("RecyclerView") int position) {
+        long delay=  Common.calculateDelay("12:00",alarms.get(position).getTime());
+
+        if (delay>=0) {
+            holder.alarm.setText(alarms.get(position).getTime()+" PM");
+        }
+        else {
+            holder.alarm.setText(alarms.get(position).getTime()+" Am");
+        }
+        if (alarms.get(position).getStatus().equals("on"))
+        {
+            holder.alarm.setChecked(true);
+        }
+        holder.alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onclick(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return alarms.size();
+    }
+
+    public class help extends RecyclerView.ViewHolder
+    {
+
+        Switch alarm;
+        public help(@NonNull View itemView) {
+            super(itemView);
+            alarm=itemView.findViewById(R.id.switchTime);
+        }
+    }
 }
--- app/src/main/java/com/example/dose/User/Alarm/Alarms.java
+++ app/src/main/java/com/example/dose/User/Alarm/Alarms.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +12,13 @@ import android.view.ViewGroup;
 
 import com.example.dose.Models.Alarm;
 import com.example.dose.R;
+import com.example.dose.User.Adapter.DisplayAlarms;
+import com.example.dose.User.Adapter.DisplayPharmaAdapter;
 import com.example.dose.User.Alarm.AddAlarm;
 import com.example.dose.User.UserHome;
 import com.example.dose.databinding.FragmentAlarmsBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,6 +33,7 @@ public class Alarms extends Fragment {
     private DatabaseReference database;
     private String userId;
     private ArrayList<Alarm> alarms;
+    private DisplayAlarms adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,12 +49,31 @@ public class Alarms extends Fragment {
         database= FirebaseDatabase.getInstance().getReference("alarms");
         userId= FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
     }
+    private void init()
+    {
+        alarms=new ArrayList<>();
+        adapter=new DisplayAlarms(alarms);
+        mBinding.alarms.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mBinding.alarms.setAdapter(adapter);
+    }
     private void getAlarm()
     {
         database.child(userId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-
+                if (snapshot.exists())
+                {
+                    alarms.clear();
+                    for (DataSnapshot snap:snapshot.getChildren())
+                    {
+                        String name=snap.child("name").getValue().toString();
+                        String time=snap.child("time").getValue().toString();
+                        String status=snap.child("status").getValue().toString();
+                        String alarmId=snap.child("alarmId").getValue().toString();
+                        alarms.add(new Alarm(name,time,status,alarmId,null));
+                    }
+                }
+                adapter.notifyDataSetChanged();
             }
 
             @Override
@@ -57,6 +82,32 @@ public class Alarms extends Fragment {
             }
         });
     }
+    private void changeStatus()
+    {
+        adapter.setOnItemClickListener(new DisplayAlarms.OnItemClickListener() {
+            @Override
+            public void onclick(int position) {
+                String id=alarms.get(position).getAlarmId();
+                String status=alarms.get(position).getStatus();
+                if (status.equals("on"))
+                    status="off";
+                else
+                    status="on";
+
+                updateStatus(id,status);
+            }
+        });
+    }
+
+    private void updateStatus(String id, String status) {
+        database.child(userId).child(id).child("status").setValue(status).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+            }
+        });
+    }
+
     private void back()
     {
         mBinding.back.setOnClickListener(new View.OnClickListener() {
